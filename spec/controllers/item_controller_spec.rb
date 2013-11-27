35c17efdbcb3c968f2979f259dd90742bba74638
ookawa <clc.iguto@gmail.com>
2013-11-28 00:19:00 +0900

    ItemContollerの作成

---
+++ spec/controllers/item_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ItemController do
+  describe '#index' do
+    before { get :index }
+    it { expect(assigns[:items]).to eq(Item.all) }
+    it { expect(response).to be_success }
+    it { expect(response).to rendet_template(:index) }
+  end
+end
