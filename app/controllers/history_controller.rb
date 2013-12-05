de5e2bb3b42a511f816781dffd575c0ecc2ec126
Akihiko Horiuchi <12ff5b8@gmail.com>
2013-12-06 00:00:40 +0900

    close #26

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -2,4 +2,8 @@ class HistoryController < ApplicationController
   def index
     @histories = History.all
   end
+
+  def show
+    @history = History.find(params[:id])
+  end
 end
--- spec/controllers/history_controller_spec.rb
+++ spec/controllers/history_controller_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 describe HistoryController do
+  fixtures :histories
   describe '#index' do
     before { get :index }
     it { expect(assigns[:histories]).to eq(History.all) }
     it { expect(response).to be_success }
     it { expect(response).to render_template(:index) }
+  end
+  describe '#show' do
+    before { get :show, id: 1 }
+    it { expect(assigns[:history]).to be_a_kind_of(History) }
+    it { expect(response).to be_success }
+    it { expect(response).to render_template(:show) }
   end
 end
