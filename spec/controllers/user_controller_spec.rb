489f38a2608557921306252f737d7d855881f751
Akihiko Horiuchi <12ff5b8@gmail.com>
2013-12-09 11:18:59 +0900

    テストコードを修正

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -32,18 +32,19 @@ describe UserController do
       it 'ユーザ一覧ページへリダイレクトする' do
         expect(response).to redirect_to user_path
       end
-      it 'ユーザレコードが更新される' do
-        expect(assigns[:user]).to be_a_kind_of(User)
+      it 'ユーザレコードが作成される' do
+        expect(assigns[:user]).to eq(User.last)
         expect(assigns[:result]).to be_true
+      end
+      it 'ユーザレコードが1件増える' do
+        expect { post :create, user: @user_param }.to change(User, :count).by(1)
       end
     end
     context '失敗時' do
       before { post :create, user: @user_param.merge(category: 3) }
       it { expect(response).to render_template(:add) }
       it { expect(response).to be_success }
-      it 'ユーザレコードが更新される' do
-        expect(assigns[:result]).to be_false
-      end
+      it { expect(assigns[:result]).to be_false }
     end
   end
   describe '#show' do
