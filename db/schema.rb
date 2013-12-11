cb7b1d4e1ea43874371ae69f88c4f3eff3da3d07
clc.iguto@gmail.com <clc.iguto@gmail.com>
2013-12-11 20:30:56 +0900

    仮想マシンで動作しない設定を排除

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20131203021646) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  #enable_extension "plpgsql"
 
   create_table "histories", force: true do |t|
     t.integer  "user_id"
