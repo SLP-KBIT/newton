e5fdbaad948bad571f05048001b2d4f77288d68e
codegeasswindow@yahoo.co.jp <codegeasswindow@yahoo.co.jp>
2013-12-13 20:46:04 +0900

    item_cntroller内にupdateメソッドの追加

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -17,4 +17,14 @@ class ItemController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def update
+ 
+    @item = Item.where(id: params[:item][:id]).first
+    @item.attributes = params.require(:item).permit(:name, :attachments, :lending_period, :category, :place, :amount, :trashed, :picture_path)
+    @result = @item.save
+    redirect_to item_path and return if @result
+    render 'edit' and return
+  end
+
 end
