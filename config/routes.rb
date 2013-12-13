9c5a163c69078c17485e2f6ac155c0ffb542f8cd
codegeasswindow@yahoo.co.jp <codegeasswindow@yahoo.co.jp>
2013-12-13 20:47:49 +0900

    updateに関する記述の追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Sample::Application.routes.draw do
   get   '/item'             => 'item#index'
   get   '/item/create'      => 'item#add'
   post  '/item/create'      => 'item#create'
+  patch '/item/update'      => 'item#update'
   get   '/item/:id'         => 'item#show'
 
   get   '/history'          => 'history#index'
