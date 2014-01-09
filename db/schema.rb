a711a5908da00f020e11f55e8eb47aebad343e5a
taniyu <s11t250@stmail.eng.kagawa-u.ac.jp>
2014-01-09 21:14:15 +0900

    テストでこけるので原因となる場所をコメントアウト

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20131218035549) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  #enable_extension "plpgsql"
 
   create_table "histories", force: true do |t|
     t.integer  "user_id"
