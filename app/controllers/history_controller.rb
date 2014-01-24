726c8a017c7c79f4ffd92e7976668f5d72615ba6
nagaon <s11t256@stmail.eng.kagawa-u.ac.jp>
2014-01-24 20:17:18 +0900

    url追加する予定

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -6,4 +6,12 @@ class HistoryController < ApplicationController
   def show
     @history = History.where(id: params[:id]).first
   end
+
+  def lend_create
+    @history = History.new(params.require(:history).permit(:item_id, :status, :amount, :created_at, :failure_detail, :detail_id))
+    @result = @history.save
+    redirect_to :controller => 'user', :action => 'mainpage', :id => 1 and return if @result
+    #render 'lend_add' and return
+    #redirect_to :action => 'mainpage', :id => 1
+  end
 end
