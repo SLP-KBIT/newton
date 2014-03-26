4ed9ded8c77e7696ce433d4897162df16cdbff57
Akihiko Horiuchi <12ff5b8@gmail.com>
2014-03-26 19:04:16 +0900

    ログインをチェックする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :login_check, unless: :login_controller?
+  before_action :current_user
+
   def current_user
     @current_user ||= find_user_from_session
   end
@@ -14,9 +17,17 @@ class ApplicationController < ActionController::Base
   end
   helper_method :permitted_user?
 
+  def login_check
+    redirect_to login_path  if current_user.nil?
+  end
+
   private
 
   def find_user_from_session
     User.id_is(session[:user_id]).first if session[:user_id]
+  end
+
+  def login_controller?
+    controller_name == 'login'
   end
 end
