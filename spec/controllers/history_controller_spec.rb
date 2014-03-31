f326c395c2ad0f6dd90a948cd13cf3366576a023
Akihiko Horiuchi <12ff5b8@gmail.com>
2014-03-31 13:22:57 +0900

    インデントと改行を修正

--- spec/controllers/history_controller_spec.rb
+++ spec/controllers/history_controller_spec.rb
@@ -4,12 +4,14 @@ require 'spec_helper'
 describe HistoryController do
   fixtures :histories
   render_views
+
   describe '#index' do
     before { get :index }
     it { expect(assigns[:histories]).to eq(History.all) }
     it { expect(response).to be_success }
     it { expect(response).to render_template(:index) }
   end
+
   describe '#show' do
     before { get :show, id: 1 }
     it { expect(assigns[:history]).to be_a_kind_of(History) }
@@ -18,11 +20,10 @@ describe HistoryController do
   end
 
   describe '#lend_add' do
-    before { get :lend_add, page:{"2"=>"2","3"=>"3"}  }
+    before { get :lend_add, page:{"2" => "2", "3" => "3"} }
     it { expect(assigns[:items]).to eq(Item.where(id: [2, 3])) }
     it { expect(response).to be_success }
     it { expect(response).to render_template(:lend_add) }
-
   end
 
   describe '#lend_create' do
@@ -31,17 +32,16 @@ describe HistoryController do
         "1" => "1"
       }
       @history_param_false = {
-         '20' => '1'
+         "20" => "1"
       }
-
     end
     context '成功時' do
       # before { post :lend_create, item: @history_param }
       pending('まだページができていないのでテストしない') do
         it '借り物リストへリダイレクトする' do
-#          post :lend_create, history: @history_param
-          post :lend_create, item: @history_param          
-          expect(response).to redirect_to :action => 'mainpage', :id => 1
+          # post :lend_create, history: @history_param
+          post :lend_create, item: @history_param
+          expect(response).to redirect_to action: 'mainpage', id: 1
         end
       end
       pending('エラーの解消法を検討中のためテストしない') do
@@ -53,37 +53,40 @@ describe HistoryController do
       it '貸出レコードが一件増える' do
         post :lend_create, item: @history_param
         expect { post :lend_create, item: @history_param }.to change(History, :count).by(1)
-      end  
+      end
       end
     end
     context '失敗時' do
-#      before { post :lend_create, history: @history_param_false }
+      # before { post :lend_create, history: @history_param_false }
       pending('エラーの解消法を検討中のためテストしない') do
-      before { post :lend_create, item: @history_param_false }
-      it { expect(assigns[:result]).to be_false }
-      it { 
-        # p body: response.body
-        get :lend_add
-        expect(response).to render_template(:lend_add) 
-      }
-      # it { expect(response).to be_success }
-    end
+        before { post :lend_create, item: @history_param_false }
+        it { expect(assigns[:result]).to be_false }
+        it {
+          # p body: response.body
+          get :lend_add
+          expect(response).to render_template(:lend_add)
+        }
+        # it { expect(response).to be_success }
+      end
     end
   end
+
   describe '#lend_confirm' do
-    before { get :lend_confirm, item:{"2"=>"1","3"=>"2"}  }
+    before { get :lend_confirm, item:{"2" => "1", "3" => "2"} }
     it { expect(assigns[:items]).to eq(Item.where(id: [2, 3])) }
     it { expect(response).to be_success }
     it { expect(response).to render_template(:lend_confirm) }
   end
+
   describe '#return_add' do
-    before { post :return_add, page:{"0"=>"5","1"=>"6"}  }    
+    before { post :return_add, page:{"0" => "5", "1" => "6"} }
     it { expect(assigns[:histories]).to eq(History.where(id: [5, 6])) }
     it { expect(response).to be_success }
     it { expect(response).to render_template(:return_add) }
   end
+
   describe '#return_confirm' do
-    before { post :return_confirm, state:{"5"=>"1", "6"=>"5"}  }    
+    before { post :return_confirm, state:{"5" => "1", "6" => "5"} }
     it { expect(assigns[:histories]).to eq(History.where(id: [5, 6])) }
     it { expect(response).to be_success }
     it { expect(response).to render_template(:return_confirm) }
