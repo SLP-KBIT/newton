d80ea8b7c2df84ad508507aaef7ff3c75edbad77
Club11 s11t229 KUMANO Keima <s11t229@stmail.eng.kagawa-u.ac.jp>
2014-05-02 22:28:39 +0900

    close #15

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -10,12 +10,16 @@ class MessageController < ApplicationController
   end
 
   def create
-    if params[:page].nil? != true
+    unless params[:page].nil?
       params[:page].each do |key, value|
         @new_message = Message.new
         @user = User.where(id: key).first
         @new_message.user_id  = @user.id
-        @new_message.subject  = params[:message].fetch('subject')
+        if params[:message].fetch('subject').present?
+          @new_message.subject  = params[:message].fetch('subject')
+        else
+          @new_message.subject  = '件名なし'
+        end
         @new_message.contents = params[:message].fetch('comtent')
         @new_message.read_flag = false
         @new_message.from = '管理者'
@@ -24,5 +28,13 @@ class MessageController < ApplicationController
       redirect_to message_path, notice: 'メッセージを送信しました' and return
     end
     redirect_to message_add_path, alert: 'チェックボックスでメッセージを送るユーザを選択してください' and return
+  end
+
+  def show
+    @message = Message.find(params[:id])
+    @edit_message = Message.where(id: params[:id]).first
+    @edit_message.read_flag = true
+    @result = @edit_message.save
+    #redirect_to message_path and return unless @result
   end
 end
