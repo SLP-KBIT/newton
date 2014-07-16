6e8a91a5a1f9dbc065344dfbfd57b2a8660e4019
nagaon <s11t256@stmail.eng.kagawa-u.ac.jp>
2014-07-16 23:21:24 +0900

    必要な項目が出るようになった。

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,47 +1,39 @@
+# -*- coding: utf-8 -*-
 class API < Grape::API
   format :json
 
-  resource :items do
+  resource 'history/lend' do
 
-   # User.where(uid: "takamatsu")
-   user = User.where(uid: params[users][uid])
-   
-    get '/history/lend' do {
-      name: user.history
-      # name: "@users.name",
-      # amount: "@histories.amount",
-      # return_date: "@histories.return_date"
-    } 
-    end
+    users = User.where(uid: "takamatsu").first
+    # users = User.where(uid: params[:users][:uid])
 
-    get '/url' do {
-      url: "history_return_add_path(id: uid)"
-    }
-    end
+    get do
+      return { result: 1 } if users.blank? 
+      
+      @item_ids = History.where(user_id: 1).order(:created_at).pluck(:item_id).uniq
+      @histories = History.where(user_id: 1).where.not('type = ?', 'ReserveHistory')
+      # @item_ids = History.where(user_id: params[:users][:id]).order(:created_at).pluck(:item_id).uniq
+      # @histories = History.where(user_id: params[:users][:id]).where.not('type = ?', 'ReserveHistory')
 
-    get '/result' do {
-      resulte: "/user/mainpage/(id: uid)"
-    }
+      @lending_item_ids = []
+      @history_ids = []
+
+      @item_ids.each do |id|
+        @history = @histories.where(item_id: id).order(:created_at).reverse_order.first      
+        if @history.present? && @history.type == 'LendHistory'
+          @history_ids.push(@history.id)
+          @lending_item_ids.push(@history.item_id)
+        end
+      end
+      @histories = History.where(id: @history_ids)
+      @items = Item.where(id: @lending_item_ids)
+
+      tmp = {}
+      @histories.each_with_index do |history, i|
+        tmp[i] = { :item_name => history.item.name, :amount => history.amount, :history_return_date => history.return_date, :result => 0 }
+      end
+      return tmp
     end
 
   end
-
 end
-
-
-    # get '/history/lend' do {
-    #   name: "user.name",
-    #   amount: "history.amount",
-    #   return_date: "history.return_date"
-    # }
-    # end
-
-    # get '/url' do {
-    #   url: "history_return_add_path(id: uid)",
-    # }
-    # end
-
-    # get '/result' do {
-    #   resulte: "/user/mainpage/(id: uid)"
-    # }
-    # end
